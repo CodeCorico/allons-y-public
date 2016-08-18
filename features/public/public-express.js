894c12e863c15eb9c240c9342b40c71bb4901a73
Xavier Boubert <xavier@boubert.fr>
2016-08-18 12:10:45 +0200

    feat(public): create the feature

---
+++ features/public/public-express.js
@@ -0,0 +1,60 @@
+'use strict';
+
+module.exports = ['$server', function($server) {
+  if (process.env.PUBLIC && process.env.PUBLIC == 'false') {
+    return;
+  }
+
+  var path = require('path'),
+      fs = require('fs'),
+      publicFolder = path.resolve('public'),
+      minified = !process.env.PUBLIC_MINIFIED || process.env.PUBLIC_MINIFIED == 'true',
+      fileName = path.basename(__filename),
+      projectName = fileName.split('-')[0],
+      projectPublicFolder = path.join(publicFolder, projectName);
+
+  function _sendFile(req, res, filename) {
+    var sendFile = false;
+
+    if (fs.existsSync(filename)) {
+      res.sendFile(filename);
+      sendFile = filename;
+    }
+    else {
+      res
+        .status(404)
+        .json({
+          error: 'File not found'
+        });
+    }
+
+    var log = {
+      req: req,
+      res: res,
+      _debugFile: __filename
+    };
+
+    if (sendFile) {
+      log.sendFile = sendFile;
+    }
+  }
+
+  $server.use('/favicon.ico', function(req, res) {
+    _sendFile(req, res, path.join(projectPublicFolder, 'favicon.ico'));
+  });
+
+  $server.use('/public', function(req, res) {
+    var filename = path.join(publicFolder, req.path),
+        extension = filename.split('.').pop();
+
+    if (minified && (extension == 'js' || extension == 'css')) {
+      var filenameMin = filename.substr(0, filename.length - extension.length) + 'min.' + extension;
+
+      if (fs.existsSync(filenameMin)) {
+        filename = filenameMin;
+      }
+    }
+
+    _sendFile(req, res, filename);
+  });
+}];
